21c44d2306f420a013492803603c2fceb4ccbccc
bren233 <ba1005748@OTC.edu>
2025-02-25 09:51:31 -0600

    High score Assignment
    High Score

---
+++ High score/High score/Program.cs
@@ -0,0 +1,20 @@
+﻿class player
+{
+    private string initals { get; set; }
+    private int score { get; set; }
+    public string Initals { get; set; }
+    public int Score { get; set; }
+
+    public player(string initals, int score)
+    {
+        this.Initals = initals;
+        this.Score = score;
+    }
+
+
+    static void Main()
+    {
+        player bill = new player("B", 300);
+        Console.WriteLine($"player {bill.Initals} score {bill.Score}");
+    }
+}
