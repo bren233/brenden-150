823fb1624f9f9e290ec250f6c7086bd0f4562d31
bren233 <ba1005748@OTC.edu>
2025-02-27 08:56:45 -0600

    Json guide Lab
    Guide lab for using JSON

---
+++ JSON Guided Lab/JSON Guided Lab/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            Name = "Billy Bob",
+            Age = 23
+        };
+
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine("Serilized JSON " + json);
+
+        Person deserializePerson = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine("Desialized Person Name " + deserializePerson.Name + " Age - " + deserializePerson.Age);
+
+    }
+}
